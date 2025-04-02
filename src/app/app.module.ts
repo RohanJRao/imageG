962defaeb9131b849663072868262a03798c0dfe
RohanJRao <rohanrao360@gmail.com>
2025-04-03 00:48:51 +0530

    completed angularpart1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 import { ImageUploaderComponent } from './components/image-uploader/image-uploader.component';
 import { ImageViewerComponent } from './components/image-viewer/image-viewer.component';
-import { ResultViewerComponent } from './components/result-viewer/result-viewer.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageUploaderComponent,
-    ImageViewerComponent,
-    ResultViewerComponent
+    ImageViewerComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
